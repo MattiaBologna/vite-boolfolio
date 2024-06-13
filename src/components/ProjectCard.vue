1564043e949e7e9a9da72c1a89c7edd4982e85df
MattiaBologna <mattiabologna01@gmail.com>
2024-06-14 01:20:10 +0200

    Add: Project detail route and component

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,12 +1,16 @@
 <template>
-    <li>
-        <h2>{{ project.title }}</h2>
-        <p>{{ project.description }}</p>
-        <div class="d-flex gap-2">
-            <div v-for="technology in project.technologies" class="btn btn-dark">
-                {{ technology.name }}
+    <li class="d-flex justify-content-between align-items-center">
+        <div class="project-infos">
+            <h2>{{ project.title }}</h2>
+            <p>{{ project.description }}</p>
+            <div class="d-flex gap-2">
+                <div v-for="technology in project.technologies" class="btn btn-dark">
+                    {{ technology.name }}
+                </div>
             </div>
         </div>
+        <router-link :to="{ name: 'projects.show', params: { slug: project.slug } }"
+            class="btn btn-primary project-detail-btn">Detail</router-link>
     </li>
 </template>
 
--- src/js/router.js
+++ src/js/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppMain from "../components/AppMain.vue";
 import AppHome from "../pages/AppHome.vue";
 import AppContact from "../pages/AppContact.vue";
+import AppProject from "../pages/AppProject.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
             path: '/contact',
             name: 'contact',
             component: AppContact
+        },
+        {
+            path: '/portfolio/:slug',
+            name: 'projects.show',
+            component: AppProject
         }
 
     ]
