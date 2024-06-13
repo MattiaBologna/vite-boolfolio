a1ece487d2a7cb01a8f355bae6a990f8600e54ba
MattiaBologna <mattiabologna01@gmail.com>
2024-06-13 18:05:35 +0200

    Add: contact form

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="container">
         <h2>Contact</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore fuga beatae aliquid itaque, excepturi
-            dolorum?</p>
+        <form action="" method="GET">
+            <div class="mb-3">
+                <label for="name" class="form-lable">Name</label>
+                <input type="text" name="name" class="form-control" id="name" placeholder="Insert name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-lable">Email</label>
+                <input type="mail" name="email" class="form-control" id="email" placeholder="Insert email">
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-lable">Message</label>
+                <input type="text" name="message" class="form-control" id="message" placeholder="Insert message">
+            </div>
+        </form>
     </div>
 </template>
 
