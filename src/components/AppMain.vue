b134bb6e73e42a4798516879813566541287e186
MattiaBologna <mattiabologna01@gmail.com>
2024-06-15 19:35:19 +0200

    Add: paginate select

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,16 +2,26 @@
     <div class="container">
         <div class="portfolio-nav d-flex justify-content-between align-items-center">
             <h2 class="display-2 mb-5">Projects</h2>
-            <input v-model="searchProject" type="text" placeholder="Search project" class="form-control"
-                style="max-width: 400px;">
+            <input v-model="searchProject" style="max-width: 500px;" type="text" placeholder="Search project"
+                class="form-control">
         </div>
         <ul class="list-unstyled">
-            <ProjectCard v-for="project in filteredProjects" key="project.id" :project="project" class="mb-5" />
+            <ProjectCard v-for="project in filteredProjects" key="project.id" :project="project" class="mb-5"
+                @change="fetchProjects" />
         </ul>
-        <ul class="d-flex gap-3 mt-2 list-unstyled">
-            <li :class="n === currentPage ? 'btn-dark' : 'btn-light'" class="btn" @click="changePage(n)"
-                v-for="n in lastPage" :key="n">{{ n }}</li>
-        </ul>
+        <div class="pagination_selectors d-flex gap-5 justify-content-between align-items-start mb-4">
+            <div class="per_page_select d-flex gap-3">
+                <select v-model="projectsPerPage" name="projects_pagination" id="projects_pagination"
+                    class="form-select" style="max-width: 100px;" @change="fetchProjects">
+                    <option v-for="n in 10" :key="n" :value="n">{{ n }}</option>
+                </select>
+                <label for="projects_pagination">Projects per page</label>
+            </div>
+            <ul v-if="!searchProject || projects.length > 6" class="d-flex gap-3  list-unstyled">
+                <li :class="n === currentPage ? 'btn-dark' : 'btn-light'" class="btn" @click="changePage(n)"
+                    v-for="n in lastPage" :key="n">{{ n }}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -28,11 +38,13 @@ export default {
             projects: [],
             lastPage: null,
             currentPage: 1,
-            searchProject: ''
+            searchProject: '',
+            projectsPerPage: 10
         }
     },
     computed: {
         filteredProjects() {
+            // TODO only looks in the page you're currently displaying
             if (!this.searchProject) {
                 return this.projects
             }
@@ -44,7 +56,8 @@ export default {
         fetchProjects() {
             axios.get('http://127.0.0.1:8000/api/projects', {
                 params: {
-                    page: this.currentPage
+                    page: this.currentPage,
+                    per_page: this.projectsPerPage
                 }
             }).then(res => {
                 // console.log(res.data.projects.data)
